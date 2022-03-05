5fedc054b72fa40669783fa2921c5a9bdfe78869
dorinebertonTlse <dorineberton@gmail.com>
2022-03-05 07:52:07 +0100

    modify navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { User } from 'src/app/interfaces/user';
-
+import axios from 'axios';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -14,5 +14,15 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
     this.user = this.authService.getCurrentUser()
+  }
+  loginOut(): void {
+    console.log('user', this.user)
+    axios.post(
+      'http://localhost:8080/logout',
+      this.user
+      )
+      .then(response => {
+      console.log('recup', response.data);
+    });
   }
 }
